c41c730bee27945f2f831725b4d63bd4ff2ce9f1
Charles Bevan <cfbevan@gmail.com>
2022-09-12 13:13:53 -0400

    initial setup

---
+++ genday.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+day=${1##+(0)}
+project=$(printf "day%02d" $1)
+session="$AOC_SESSION"
+
+cargo new ${project}
+
+cd ${project}
+
+# get input
+curl -s "https://adventofcode.com/2015/day/${day}/input" --cookie "session=${AOC_SESSION}" -o input.txt
+
+
+echo -n 'fn main() {
+    let data = include_str!("../input.txt").trim();
+    println!(
+        "Part 1: {}",
+        ""
+    );
+
+    println!(
+        "Part 2: {}",
+        ""
+    );
+}' > src/main.rs
